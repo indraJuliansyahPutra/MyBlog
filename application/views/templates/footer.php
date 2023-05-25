1c17c0a94c7f218c1e1fbdcc87be9265cfed3ceb
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2023-05-25 21:25:30 +0700

    footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,31 @@
+    
+    <footer>
+        <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                  <h4>About Us</h4>
+                  <p>Share Your Voices with the world</p>
+              </div>
+              <div class="col-md-4">
+                  <h4>Related Links</h4>
+                  <ul>
+                    <li><a href="<?php echo base_url('posts'); ?>">Latest Post</a></li>
+                    <li><a href="<?php echo base_url('categories'); ?>">Category</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-4">
+                  <h4>Our Contact</h4>
+                  <p>Email: info@MyBlog.com</p>
+                  <p>Telepon: 123-456-7890</p>
+                  <div class="social-media-icons">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </footer>
+      <script src="<?php echo base_url(); ?>assets/js/post.js"></script>
+    </body>
+  </html>
