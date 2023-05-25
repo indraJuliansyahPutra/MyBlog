cd86138ac276f9ab9f3b410a88b0a187407377ce
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2023-05-25 21:28:26 +0700

    about

---
+++ application/views/pages/about.php
@@ -0,0 +1,98 @@
+<div class="container">
+    <h2>
+        <?= $title ?>
+    </h2>
+    <div style="text-align:center;">
+        <img src="<?php echo site_url(); ?>assets/images/attribute/Logo.png" alt="Your Name" style="max-width:25%; height:auto;"
+            id="logo">
+    </div>
+    <p>This is MyBlog version 1.0</p>
+    <p>MyBlog is a website project created to fulfill the requirements of the Web Programming II course. Our platform is
+        designed to be a space for bloggers and writers to share their ideas, experiences, and stories with the world.
+        Our mission is to create a community of like-minded individuals who are passionate about writing and want to
+        make an impact.
+    </p>
+    <p>With MyBlog, you can create your own blog, publish your articles, connect with other writers, and grow your
+        audience. We offer a user-friendly platform that makes it easy to get started, regardless of your level of
+        experience.
+    </p>
+    <p>Thank you for choosing MyBlog as your platform for sharing your voice with the world. We can't wait to see what
+        you create!
+    </p>
+    <div class="container">
+        <div class="best-features about-features">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-heading">
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="right-image">
+                            <img src="<?php echo site_url(); ?>assets/images/attribute/About.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="left-content">
+                            <h4>Lorem ipsum dolor sit amet.</h4>
+                            <p>Blog has become an important platform in the digital and social media world. It can help
+                                build trust and relationships with readers, as well as encourage interaction and
+                                participation from site visitors. In today's information age, blogs can be an important
+                                source of information and inspiration for seeking insights and understanding on
+                                particular topics. <br><br> In addition, we also provide comment and share features, so readers
+                                can provide feedback and share your articles on various social media platforms. Thus,
+                                your article can not only help shape readers' opinions and views, but also increase
+                                engagement and exposure to your brand.
+                            </p>
+                            <ul class="social-icons">
+                                <button type="button" class="btn btn-secondary" fdprocessedid="mop95"><a href="#"><i
+                                            class="fab fa-facebook"></i></a></button>
+                                <button type="button" class="btn btn-secondary" fdprocessedid="mop95"><a href="#"><i
+                                            class="fab fa-twitter"></i></a></button>
+                                <button type="button" class="btn btn-secondary" fdprocessedid="mop95"><a href="#"><i
+                                            class="fab fa-instagram"></i></a></button>
+                                <button type="button" class="btn btn-secondary" fdprocessedid="mop95"><a href="#"><i
+                                            class="fab fa-youtube"></i></a></button>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="team-members">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <h2>Lorem ipsum dolor sit amet.</h2>
+                    </div>
+                    <h5>Lorem ipsum dolor sit amet.</h5>
+                    <p>We invite you to join us and write your articles on our blog website. We believe that your
+                        knowledge
+                        and experience are valuable and can provide benefits to our loyal readers. By writing on our
+                        blog
+                        website, you can not only share your insights and views on interesting topics, but also expand
+                        your
+                        reach and gain recognition from a wider audience.<br><br>We provide an easy-to-use and
+                        user-friendly
+                        platform for you to write and publish your articles. Once your article is approved by our
+                        editorial
+                        team, you will have the opportunity to become a regular contributor and build a reputation as a
+                        trusted writer in the industry.
+                    </p>
+                    <p>In addition, we also provide comment and share features, so readers can provide feedback and
+                        share
+                        your articles on various social media platforms. Thus, your article can not only help shape
+                        readers'
+                        opinions and views, but also increase engagement and exposure to your brand.<br><br>So, what are
+                        you
+                        waiting for? Join our community and start writing your articles now!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="<?php echo base_url(); ?>assets/js/about.js"></script>
